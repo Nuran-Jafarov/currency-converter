a51c62e5a7e453096238067a6ba9a1796c137338
Nuran Jafarov <nuran.jafarov@gmail.com>
2020-11-14 21:46:17 +0100

    Structure of CurrencyListItem and removing logic is ready.

---
+++ src/components/Converter/ListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-col class="pa-3" cols="4">
+    <v-card>
+      <v-card-title class="headline">
+        {{ code }}
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="remove"><v-icon>mdi-close</v-icon></v-btn>
+      </v-card-title>
+
+      <v-card-subtitle>{{ description }}</v-card-subtitle>
+
+      <v-card-text>
+        <v-text-field v-model.number="amount" outlined></v-text-field>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "ConverterListItem",
+
+  props: {
+    code: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+      default: 0,
+    },
+    id: {
+      type: Number,
+    },
+  },
+  methods: {
+    remove() {
+      this.$emit("remove");
+    },
+  },
+};
+</script>
