ee70b6cd0ad746c3ab4013cb52e3e6a6ef52c8ef
Nuran Jafarov <nuran.jafarov@gmail.com>
2020-11-15 23:56:06 +0100

    Created logic for adding new currencies to the converter.

---
+++ src/components/Converter/ListAddItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-col class="pa-3" cols="4">
+    <v-card>
+      <v-card-title class="headline">
+        Add
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="addSelectedCurrencyCode({ code: selected })">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-subtitle>
+        Select the currency you want to add to the converter
+      </v-card-subtitle>
+
+      <v-card-text>
+        <v-autocomplete
+          v-model="selected"
+          :items="selectItems"
+          outlined
+        ></v-autocomplete>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  name: "ConverterListAddItem",
+  data() {
+    return {
+      selected: "",
+    };
+  },
+  computed: {
+    ...mapGetters("converter", ["notSelectedCurrenciesOfDate"]),
+    selectItems() {
+      const currencies = this.notSelectedCurrenciesOfDate();
+
+      return currencies.map((currency) => {
+        return {
+          text: `${currency.code} - ${currency.description}`,
+          value: currency.code,
+        };
+      });
+    },
+  },
+  methods: {
+    ...mapMutations("converter", ["addSelectedCurrencyCode"]),
+  },
+};
+</script>
--- src/store/modules/converter.js
+++ src/store/modules/converter.js
@@ -5,6 +5,7 @@ const namespaced = true;
 const state = {
   latestDate: null,
   rates: {},
+  selectedCurrencyCodes: [],
 };
 
 const getters = {
@@ -12,16 +13,45 @@ const getters = {
     if (date === "latest") return state.rates[state.latestDate];
     else return state.rates[date];
   },
+  currenciesOfDate: (state) => (date = "latest") => {
+    try {
+      if (date === "latest") return Object.keys(state.rates[state.latestDate]);
+      else return Object.keys(state.rates[date]);
+    } catch {
+      return [];
+    }
+  },
+  selectedCurrencies(state, getters, rootState) {
+    return rootState.availableCurrencies.filter((currency) =>
+      state.selectedCurrencyCodes.includes(currency.code)
+    );
+  },
+  notSelectedCurrenciesOfDate: (state, getters, rootState) => (
+    date = "latest"
+  ) => {
+    date = date === "latest" ? state.latestDate : date;
+
+    if (!(date in state.rates)) return [];
+    else
+      return rootState.availableCurrencies.filter(
+        (currency) =>
+          !state.selectedCurrencyCodes.includes(currency.code) &&
+          currency.code in state.rates[date]
+      );
+  },
 };
 
 const mutations = {
   updateRates(state, { date, rates }) {
-    if ("ARM" in rates) delete rates["ARM"];
+    if ("AMD" in rates) delete rates["AMD"];
 
     state.rates[date] = rates;
   },
   updateLatestDate(state, { date }) {
     state.latestDate = date;
+  },
+  addSelectedCurrencyCode(state, { code }) {
+    state.selectedCurrencyCodes.push(code);
   },
 };
 
